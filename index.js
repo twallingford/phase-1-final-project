21309307d98bb80092f579dae363bc2d3e41e68b
Thomas Wallingford <tbwallingford@gmail.com>
2023-03-20 13:22:26 -0600

    Added an area for the stats to pop up in and finished the fetch requests so that they actually showed the statistics requested.

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const submitYear = document.querySelector("#submit-year");
 const yearLabel = document.querySelector("#year-select-label");
 const submitForm = document.querySelector("#submit-form");
 const drilldown = document.querySelector("#drilldown");
+const stateSelect = document.querySelector("#state-select");
+const statsArea = document.querySelector("#stats-area");
 
 yearLabel.innerText = `Select a year between ${dateMin} and ${dateMax}.`;
 
@@ -19,14 +21,19 @@ submitForm.addEventListener("submit", (e) => {
         fetch(`https://datausa.io/api/data?drilldowns=${drilldown.value}&measures=Population&year=${submitYear.value}`)
         .then((response) => response.json())
         .then((d) => {
-            console.log("Nation");
+            statsArea.innerText = `The population of the nation in the year ${submitYear.value} was ${d.data[0]["Population"]}!`
         })
     }
     else{
         fetch(`https://datausa.io/api/data?drilldowns=State&measures=Population&year=${submitYear.value}`)
         .then((response) => response.json())
         .then((d) => {
-            console.log("State");
+            for(const state in d.data){
+                if(d.data[state]["State"] === stateSelect.value){
+                    statsArea.innerText = `The population of ${stateSelect.value} in ${submitYear.value} was ${d.data[state]["Population"]}!`
+                }
+            }
+
         })
     }
 })

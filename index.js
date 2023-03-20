1432877dfad934075b3cbd497347a1f186cbcb4e
Thomas Wallingford <tbwallingford@gmail.com>
2023-03-20 12:34:24 -0600

    Made sure JS was deferred. Added JS functionality to add the correct years to the year selector.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ I just put whatever the current year is minus a certain number. I'm not 100% sur
 but it would be more like what I'd use in the real world than setting the variables to 2013 and 2020*/
 const dateMin = new Date().getFullYear() - 10;
 const dateMax = new Date().getFullYear() - 3;
+
+/*Variables for locations in the HTML*/
+const submitYear = document.querySelector("#submit-year");
+const yearLabel = document.querySelector("#year-select-label");
+
+yearLabel.innerText = `Select a year between ${dateMin} and ${dateMax}.`;
+
+submitYear.setAttribute("min", dateMin);
+submitYear.setAttribute("max", dateMax);
+submitYear.setAttribute("value", dateMin);
